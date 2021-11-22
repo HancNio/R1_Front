ea8237577ef42369483212e9b776607e48c9ec0f
HugoNinoMT <hugo.nino.mt@correo.usa.edu.co>
2021-11-22 12:44:09 -0500

    Ajustes validaciones JS

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -2,6 +2,8 @@
 
 // validaciones campos registrar 
 $("#formularioReg").validate({
+    onkeyup: true,
+    onfocusin: true,
     rules: {
         name: {
             required: true,
@@ -17,12 +19,12 @@ $("#formularioReg").validate({
         password: {
             required: true,
             minlength: 6,
-            maxlength: 16,
+            maxlength: 50,
         },
         password_confirm: {
             required: true,
             minlength: 6,
-            maxlength: 16,
+            maxlength: 50,
             equalTo: "#password",
         }
     }
@@ -38,49 +40,61 @@ $("#registrar").click(function () {
     let password_confirm = $("#password_confirm").val()
 })// fin validaciones 
 
-//// Post
+//// Guardar usuario
 function guardarUsuarios() {
     var email = $("#email").val();
 
     $.ajax({
-        url: "http://152.70.222.238:8081/api/user/"+email,
+        url: "http://localhost:8081/api/user/" + email,
         type: "GET",
         datatype: "JSON",
         success: function (response) {
             console.log(response)
             if (response == true) {
-                swal("El usuario ya existe, valide los datos o ingrese al sistema por el Login","Validación Incorrecta", "error");
+                swal.fire({
+                    title: "El usuario ya existe",
+                    text: "Valide por favor",
+                    icon: "error",
+                    showConfirmButton: true,
+                    confirmButtonColor: "#242729",
+                    confirmButtonText: "Aceptar",
+                    footer: "<a href='index.html'>o de clic aquí ingresar <i class='fas fa-sign-in-alt heart'></a>"
+                });
             } else {
                 let var1 = {
                     email: $("#email").val(),
                     password: $("#password").val(),
                     name: $("#name").val()
-                };              
+                };
                 $.ajax({
                     type: 'POST',
                     contentType: "application/json; charset=utf-8",
                     dataType: 'JSON',
                     data: JSON.stringify(var1),
-                    url: "http://152.70.222.238:8081/api/user/new",
+                    url: "http://localhost:8081/api/user/new",
                     success: function (response) {
                         console.log(response);
                         console.log("Se guardo correctamente");
-                        swal("Cuenta creada de forma correcta","Validación Correcta", "success");
+                        swal.fire({
+                            title: "Cuenta creada de forma correcta",
+                            text: "Bienvenido " + response.name,
+                            icon: "success",
+                            showConfirmButton: false,
+                            allowOutsideClick: true,
+                            footer: "<a href='index.html'>Ingresar <i class='fas fa-sign-in-alt heart'></i></a>"
+                        });
                     },
                     error: function (jqXHR, textStatus, errorThrown) {
-                        swal("No se guardo correctamente, valido los campos","Validación Incorrecta","error");
+                        swal.fire("Error en la aplicacion, comuniquese con el administrador del sistema", "Validación Incorrecta", "error");
                     }
                 });
-            }            
+            }
         },
         error: function (jqXHR, textStatus, errorThrown) {
-            swal("Validación","Error en la aplicacion, comuniquese conel administrador del sistema","error");
+            swal.fire("Error en la aplicacion, comuniquese con el administrador del sistema", "Validación Incorrecta", "error");
         }
     });
-} //// fin Post
-
-
-
+} //// fin guardar usuario
 
 /*================================== Funciones formulario Login ===================================*/
 
@@ -96,7 +110,7 @@ $("#formulario").validate({
         password: {
             required: true,
             minlength: 6,
-            maxlength: 16,
+            maxlength: 50,
         }
     }
 })
@@ -109,24 +123,63 @@ $("#login").click(function () {
     let password = $("#password").val()
 })// fin validaciones
 
-//// GET 
+//// Loguearse 
 function traerUsuarios() {
     var email = $("#email").val();
+    var password = $("#password").val();
 
     $.ajax({
-        url: "http://152.70.222.238:8081/api/user/"+email,
+        url: "http://localhost:8081/api/user/" + email,
         type: "GET",
         datatype: "JSON",
         success: function (response) {
-            console.log(response)
-            if (response == true) {                
-                swal("Bienvenido "+email, "Validación Correcta", "success");
+            if (response == true) {
+                $.ajax({
+                    url: "http://localhost:8081/api/user/" + email + "/" + password,
+                    type: "GET",
+                    datatype: "JSON",
+                    success: function (response) {
+                        console.log(response)
+                        if (response.name != 'NO DEFINIDO') {
+                            swal.fire({
+                                title: "Bienvenido " + response.name,
+                                text: "Validación Correcta",
+                                icon: "success",
+                                showConfirmButton: false,
+                                allowOutsideClick: true,
+                                timer: 5000,
+                                timerProgressBar: true
+                            });
+                        } else {
+                            swal.fire({
+                                title: "La combinación usuario/contraseña es incorrecta",
+                                text: "Valide por favor",
+                                icon: "error",
+                                showConfirmButton: true,
+                                confirmButtonColor: "#242729",
+                                confirmButtonText: "Aceptar",
+                                footer: "<a href='registrar.html'>o de clic aquí para registrarse</a>"
+                            });
+                        }
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        swal.fire("Error en la aplicacion, comuniquese con el administrador del sistema","Validación Incorrecta","error");
+                    }
+                });
             } else {
-                swal("El usuario no existe, por favor valide los datos","Validación Incorrecta", "error");
-            }            
+                swal.fire({
+                    title: "El usuario no existe en el sistema",
+                    text: "Valide por favor",
+                    icon: "error",
+                    showConfirmButton: true,
+                    confirmButtonColor: "#242729",
+                    confirmButtonText: "Aceptar",
+                    footer: "<a href='registrar.html'>o de clic aquí para registrarse</a>"
+                });
+            }
         },
         error: function (jqXHR, textStatus, errorThrown) {
-            swal("Validación","Error en la aplicacion, comuniquese conel administrador del sistema","error");
+            swal.fire("Error en la aplicacion, comuniquese con el administrador del sistema","Validación Incorrecta","error");
         }
     });
-}
+}//// fin Loguearse
